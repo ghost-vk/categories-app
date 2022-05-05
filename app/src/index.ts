ac2b2ee1a5a240a8390a5f5012131da79488c3df
ghost-vk <ghost.vkv@gmail.com>
2022-05-05 22:53:19 +0500

    Swagger documentation

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,8 @@
 import 'module-alias/register'
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from '@config/openapi.json'
 
 import CategoryRouter from '@routers/category.router'
 import errorInterceptorMiddleware
@@ -14,10 +16,11 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/categories', CategoryRouter)
 
 app.all('*', (req: Request, res: Response) => {
-  res.status(404).json({ error: 'Resourse not found' })
+  res.status(404).json({ error: 'Resource not found' })
 })
 
 app.use(errorInterceptorMiddleware)
